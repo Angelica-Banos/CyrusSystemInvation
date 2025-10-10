ef1341d5e4fcf4859651aaedc1d72c7ef0114c9e
Angelica-Banos <ambanos@uninorte.edu.co>
2025-10-10 03:06:27 -0500

    Escena minijuego 4 funcionando

--- CyrusSystemInvation/Assets/Scripts/Minijuego/Plataformas/Teletransportador.cs
+++ CyrusSystemInvation/Assets/Scripts/Minijuego/Plataformas/Teletransportador.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Teletransportador : MonoBehaviour
 {
-    private Vector3 posicion = new Vector3(1f, 1f, 0.5f);
+    private Vector3 posicion = new Vector3(1f, 1.5f, 0.5f);
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
